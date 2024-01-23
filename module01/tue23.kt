d66141a3c05a0f8f19c0d87a96c50aafa8a68f81
David3677814 <157508220+David3677814@users.noreply.github.com>
2024-01-23 11:40:25 -0600

    created module 1 code

---
+++ module01/tue23.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //println("Hello, world")
+    //println("My name is David, and I'm learning kotlin")
+    println("The hero announces her presence to the world.")
+
+    val heroName:String = "Madrigal"
+
+    var playerLevel:Int = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel +=1
+    println(playerLevel)
+}
